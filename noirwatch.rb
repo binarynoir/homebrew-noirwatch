f5e04e00847e7f283f7a147db074063ca8574804
KryptonianSon <kryptonianson@users.noreply.github.com>
2024-12-20 16:58:37 -0500

    Update noirwatch.rb

--- noirwatch.rb
+++ noirwatch.rb
@@ -4,7 +4,8 @@ class Noirwatch < Formula
   url "https://github.com/binarynoir/noirwatch/archive/refs/tags/v1.3.1.tar.gz"
   sha256 "dfb6ce23d45a54f919ffed5981c026f04413668e7c66cc300cebe852be3550f5"
 
-  depends_on "bash" "coreutls"
+  depends_on "bash"
+  depends_on "coreutls"
 
   def install
     bin.install "noirwatch"
